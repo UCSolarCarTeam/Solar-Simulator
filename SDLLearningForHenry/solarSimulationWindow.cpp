0981c9d5ccc3679da6ae1973d915881f867a5a10
henrytran28 <hetran@localhost.localdomain>
2016-02-06 17:17:11 -0700

    Made code easier to read andcleaned up some unused items

--- SDLLearningForHenry/solarSimulationWindow.cpp
+++ SDLLearningForHenry/solarSimulationWindow.cpp
@@ -5,16 +5,17 @@
 #include <SDL_ttf.h>
 #include <cmath>
 
-
 // Screen dimension constants
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 800;
 const int BUTTON_WIDTH = 100;
 const int BUTTON_HEIGHT = 50;
 
+// Function declarations
 bool init();
 void close();
 void createMenu();
+SDL_Texture* createTexture();
 
 // In the future should change to textures if need performance
 SDL_Window* gWindow = NULL;
@@ -22,31 +23,33 @@ SDL_Texture* gTexture = NULL;
 SDL_Renderer* gRenderer = NULL;
 SDL_Rect button = {20, 700, BUTTON_WIDTH, BUTTON_HEIGHT};
 
+/**************************************************************/
 
-bool init()
-{
+bool init() {
 	//Initialization flag
 	bool success = true;
 
 	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+	if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
 		success = false;
-	}
-	else
-	{
+	} else {
+		//Set texture filtering to linear
+		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) ) {
+			printf( "Warning: Linear texture filtering not enabled!" );
+		}
+
 		//Create window
 		gWindow = SDL_CreateWindow( "Solar Simulation Window", SDL_WINDOWPOS_UNDEFINED,
     SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
-		{
+
+		if(gWindow == NULL) {
 			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
 			success = false;
-		}
-		else
-    {
+		}	else {
+			// Create renderer for window
       gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+
       if (gRenderer == NULL) {
         printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
         success = false;
@@ -54,23 +57,23 @@ bool init()
         // Initialize renderer color
         SDL_SetRenderDrawColor(gRenderer, 250, 250, 210, 255);
       }
-			/*Get window surface
-			gScreenSurface = SDL_GetWindowSurface( gWindow );
-      SDL_FillRect(gScreenSurface, NULL, SDL_MapRGB(gScreenSurface->format, 255  255, 255));
-      */
 		}
 	}
-
 	return success;
 }
 
-void close()
-{
-	//Destroy window
-	SDL_DestroyWindow( gWindow );
+void createTexture() {
+	
+}
+
+void close() {
+	//Destroy texture, renderer, window
+	SDL_DestroyTexture(gTexture);
   SDL_DestroyRenderer(gRenderer);
+	SDL_DestroyWindow(gWindow);
+	gTexture = NULL;
+	gRenderer = NULL;
 	gWindow = NULL;
-  gRenderer = NULL;
 
 	//Quit SDL subsystems
 	SDL_Quit();
@@ -78,16 +81,11 @@ void close()
 
 /**************************************************************************/
 
-int main( int argc, char* args[] )
-{
+int main( int argc, char* args[] ) {
 	//Start up SDL and create window
-	if( !init() )
-	{
-		printf( "Failed to initialize!\n" );
-	}
-	else
-	{
-
+	if(!init()) {
+		printf("Failed to initialize!\n");
+	} else {
 			//Main loop flag
 			bool quit = false;
 
@@ -95,14 +93,11 @@ int main( int argc, char* args[] )
 			SDL_Event e;
 
 			//While application is running
-			while( !quit )
-			{
+			while(!quit) {
 				//Handle events on queue
-				while( SDL_PollEvent( &e ) != 0 )
-				{
+				while(SDL_PollEvent(&e) != 0) {
 					//User requests quit
-					if( e.type == SDL_QUIT )
-					{
+					if(e.type == SDL_QUIT) {
 						quit = true;
 					}
 				}
