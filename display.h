1abad86b91a847ea595ce6b2e11c9b6c213e1c4a
Bryson Ding <bryson189@gmail.com>
2016-02-11 11:15:22 -0700

    Added display and mesh files

---
+++ display.h
@@ -0,0 +1,22 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <string>
+#include <SDL2/SDL.h>
+
+class Display
+{
+public:
+	Display(int width, int height, const std::string& title);
+	void Update();
+	bool IsClosed();
+	virtual ~Display();
+protected:
+private:
+	Display(const Display& other);
+	Display& operator=(const Display& other);
+	SDL_Window* m_window;
+	bool m_isClosed;
+};
+
+#endif 
