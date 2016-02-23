970080a9be74e73caa8bb993110a12ef6ebc9f75
Enoch Tsang <etsang1@hotmail.com>
2016-02-22 20:22:21 -0700

    general cleanup of mesh files

--- src/mesh.h
+++ src/mesh.h
@@ -1,29 +1,28 @@
 #ifndef MESH_H
 #define MESH_H
 
-#include <glm/glm.hpp>
 #include <GL/glew.h>
+#include <glm/glm.hpp>
 
 class Mesh
 {
-	public:
-		Mesh(glm::vec3* vertices, unsigned int numVertices, unsigned int* indices, unsigned int numIndices);
-		void Draw();
-		virtual ~Mesh();
+public:
+	Mesh(glm::vec3* vertices, unsigned int numVertices, unsigned int* indices, unsigned int numIndices);
+	virtual ~Mesh();
+	void Draw();
 
-	protected:
-	private:
-		Mesh(const Mesh& other);
-		void operator=(const Mesh& other) {}
-		enum{
-			POSITION_VB,
-			INDEX_VB,
-			NUM_BUFFERS
-		};
-
-		GLuint m_vertexArrayObject;
-		GLuint m_vertexArrayBuffers[NUM_BUFFERS];
-		unsigned int m_drawCount; //Number of drawn indices
+private:
+	Mesh(const Mesh& other);
+	void operator=(const Mesh& other) {}
+	enum
+	{
+		POSITION_VB,
+		INDEX_VB,
+		NUM_BUFFERS
+	};
+	GLuint vertexArrayObject_;
+	GLuint vertexArrayBuffers_[NUM_BUFFERS];
+	unsigned int drawCount_;
 };
 
-#endif 
+#endif
