31ae354c8f03400c55bbcf0ef7ee0a47ea89493e
Bryson Ding <bryson189@gmail.com>
2016-04-02 16:44:25 -0600

    Added stubs for PowerOutput

--- src/Logic/PowerCalculator.h
+++ src/Logic/PowerCalculator.h
@@ -3,11 +3,14 @@
 #include <GL/glew.h>
 #include <glm/glm.hpp>
 #include <vector>
+#include "../Data/ModelData.h"
 
 class PowerCalculator
 {
 public:
-
+	double calculateTotalPower(ModelData& data, glm::vec3& solarArray) const;
+	bool isOuterTriangle(glm::vec3& normalVector) const;
+	glm::vec3 getNormalVector(ModelData& data, int triangleIndex) const;
 private:
 
 };
